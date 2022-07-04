d70f973532c42ccc9e5b624bf1292f42f350a772
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-04 18:18:27 +0000

    positive or negative updated I

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
- 	if (n > 0)
+	if (n > 0)
 		printf("%d is %s\n", n, "positive");
 	else if (n == 0)
 		printf("%d is %s\n", n, "zero");
