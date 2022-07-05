0fb757a6821116553a54d9496d30d96d614fb374
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-05 14:11:49 +0000

    0._putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 on success
+ */
+int main(void)
+{
+	char *pc = "_putchar";
+	
+	while (*pc)
+	{
+		_putchar(*pc);
+		pc++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
