d599abae2386519d4f9ce179bec8bad196de672b
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-05 16:53:21 +0000

    alphabets in 10 lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function to  print out lowercase ;etters 1ox
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int floop;
+
+	for (floop = 0; floop < 10; floop++)
+	{
+		int c;
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+	}
+}
