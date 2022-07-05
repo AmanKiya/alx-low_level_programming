6ba78a4c9ebd069ca41952f38b5f30ec2751afa9
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-05 18:41:17 +0000

    absolute value of a number I

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** _abs - a function that computes the absolute value of an integer
+/** _abs - a function that computes 
+ * the absolute value of an integer
  * @num: integer input
  *
  * Return: absolute value of num
