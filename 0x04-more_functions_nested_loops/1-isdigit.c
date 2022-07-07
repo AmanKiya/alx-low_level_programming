26cc139125eaab956003199aa326fcae32244eb7
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-07 13:58:01 +0000

    is digit V

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isdigit - a function that checks for a digit 
+ * _isdigit - a function that checks for a digit
  * @c: integer input
- *
+ * 
  * Return: 1 if c is a digit and 0 otherwise
  */
 int _isdigit(int c)
