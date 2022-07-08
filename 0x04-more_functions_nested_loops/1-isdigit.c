1172928db9190e2086436783cacf7c0fae6665cc
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-08 12:00:49 +0000

    is digit second day IIIIIIIIIIIIII

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
- * _isdigit - a function that checks for a digit
- * @c: integer inpu
+ * _isdigit - checks for characters that are digits
+ * @c: character to check
  * 
- * Return: 1 if c is a digit and 0 otherwise
+ * Return: 0 or 1
  */
+
+
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-		return (0);
-
+		return (1);
 	else
 		return (0);
 }
