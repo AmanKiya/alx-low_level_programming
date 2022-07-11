a863230aa58368ba7fe51d670f579f58add97809
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-11 23:51:13 +0000

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: pointer s
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = sizeof(s);
+	return (length);
+}
