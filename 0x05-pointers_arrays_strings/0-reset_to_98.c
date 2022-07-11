affeaa31911309c5c43dd905ff19379644357801
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-11 23:32:58 +0000

    reset second time I

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,9 @@
 
 /**
  * reset_to_98 - a function that takes a pointer to an int
- * @*n: pointer nr
+ * @*n: pointer n
+ * Description: using pointer n to reset a value
+ *
  * Return: always 0.
  */
 void reset_to_98(int *n)
