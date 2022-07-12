c9a64f8cd4aa92d9e5454fc1b96968c44524b530
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-12 01:23:45 +0000

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - a function taht prints n elements of an array
+ * @a: pointer a
+ * @n: parameter n
+ * Return: always 0
+ */
+void print_array(int *a, int n)
+{
+	int i, last;
+
+	last = n - 1;
+	for (i = 0; i <= last; i++)
+	{
+		printf("%d", a[i]);
+		if (i < last)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
+
