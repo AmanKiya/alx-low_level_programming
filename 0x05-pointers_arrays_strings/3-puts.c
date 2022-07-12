2a9d9abecf434af054411d29d645746372a3fc71
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-12 00:07:14 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.c"
+
+/**
+ * _puts - a function that prints a string
+ * @str: pointer str
+ * Return: always 0
+ */
+void _puts(char *str)
+{
+	char c;
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		printf("'%c'", *(s + i));
+		i++;
+	}
+
+}
