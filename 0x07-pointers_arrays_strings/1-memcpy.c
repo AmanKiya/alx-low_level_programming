d6cde674028a5110701c5b7d669315ffd21ad1d1
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-18 11:58:26 +0000

    memory copy II

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- * _memcpy - a function that copies memory area
- * @dest: a pointer dest
- * @src: a pointer src 
- * @n: unsigned interger parameter
- * Return: a pointer dest
+ * _memcpy - fills memory with another buffer.
+ * @dest: source string
+ * @src: string for filling
+ * @n: lenght of buffer
+ * Return: new string.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
