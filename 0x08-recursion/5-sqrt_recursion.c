6bb639bb71103787799cdbecfbc377a6243c8740
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-21 02:11:47 +0000

    the square root of a natural number I

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,18 +8,18 @@
  */
 int _sqrt_recursion(int n)
 {
-	 if (n < 0)
-	 {
-	 	return (-1);
-	 }
-	 else if (n == 0 || n == 1)
-	 {
-	 	return (n);
-	 }
-	 else
-	 {
-	 	return (sqrt_manual_calc(n, 1));
-	 }
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (sqrt_manual_calc(n, 1));
+	}
 }
 
 /**
