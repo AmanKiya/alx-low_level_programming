3c74081c406d5200ea9d82323c5859824b34dd50
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-21 02:23:26 +0000

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * is_prime_number - a function that returns 1 if the input integer
+ * is a prime number, otherwise return 0.
+ * @n: integer parameter n
+ * Return: 1 for prime number input, otherwise 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n == 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (is_prime_manual(n, 2));
+	}
+}
+/**
+ * is_prime_manual - calculate if the number is prime.
+ * @n: natural number
+ * @i: counter or divisor of n.
+ *
+ * Return: 1 if is prime 0 otherwise.
+ */
+int is_prime_manual(int n, int i)
+{
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	else if (i == (n - 1) && n % i != 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (is_prime_manual(n, i + 1));
+	}
+}
