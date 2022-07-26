c71027f7e436a9efd0ea82f71f391882117e9cd8
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-26 13:09:12 +0000

    malloc iii

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 		arMem = malloc(size * sizeof(char));
 		if (arMem == NULL)
 			return (NULL);
-		for (i = 0, i < size, i++)
+		for (i = 0; i < size; i++)
 			*(arMem + i) = c;
 		return (arMem);
 }
