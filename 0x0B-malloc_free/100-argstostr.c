d253b420ccc733dc71489d544ceca894794699fa
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-07-26 14:02:24 +0000

    argsto i

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -30,12 +30,12 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; *(*(av + i) + j) != '\0'; j++)
-			{
-				*(newStr + k) = *(*(av + i) + j);
-				k++;
-			}
-			*(newStr + k) = '\n';
+		{
+			*(newStr + k) = *(*(av + i) + j);
 			k++;
+		}
+		*(newStr + k) = '\n';
+		k++;
 	}
 	*(newStr + k) = '\0';
 
