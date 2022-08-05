59d2ef0c9354e69385a0ad2da7eed96ef9dd06e0
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-08-05 01:34:35 +0000

    sum them all ii

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,6 +4,7 @@
 /**
  * sum_them_all - sum all
  * @n: given number
+ *
  * Return: the sum
  */
 int sum_them_all(const unsigned int n, ...)
@@ -12,8 +13,10 @@ int sum_them_all(const unsigned int n, ...)
 	va_list arguments;
 
 	va_start(arguments, n);
+
 	for (i = 0; i < n; i++)
 		sum = sum + va_arg(arguments, int);
+
 	va_end(arguments);
 	return (sum);
 }
