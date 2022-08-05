aa14ee108e08a956c31aeee4b6fe927afc0d3a59
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-08-05 01:36:56 +0000

    sum them all iiiii

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,10 +1,8 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
  * sum_them_all - sum all
  * @n: given number
- *
  * Return: the sum
  */
 int sum_them_all(const unsigned int n, ...)
