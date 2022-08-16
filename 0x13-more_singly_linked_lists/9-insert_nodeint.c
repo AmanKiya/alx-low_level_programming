5bcd7725ffee6f0a32adcf41eb9f5f669c5caaf0
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-08-16 17:23:45 +0000

    all projects except 0, 101 and 102 updated 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,5 +43,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		node_index->next = new_node;
 		new_node->next = node_after;
 		return (new_node);
-								}
+	}
 }
