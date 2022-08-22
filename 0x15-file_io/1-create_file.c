7d35b1467fdace855bf796c816bf0b97e47fc5e7
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-08-22 22:27:42 +0000

    create a file I

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,23 +1,25 @@
 #include "main.h"
-
 /**
- * create_file - a function that creates a file
- * @filename: the name of the file to be created
- * @text_content: a null terminated string to write to the file
- * Return: 1 on success -1 on failure
+ * create_file - creates a file
+ * @filename: name of file to be readed
+ * @text_content: the text inside the file.
+ * Return: 2 success, otherwise 0
  */
 int create_file(const char *filename, char *text_content)
 {
-	int i, fd;
-	
+	int file, i;
+
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd == -1)
+
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	for (i = 0; *(text_content + i) != '\0'; i++)
 		;
-	write(fd, text_content, i);
-	close(fd);
+	write(file, text_content, i);
+	close(file);
 	return (1);
 }
